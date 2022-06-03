117de6088afec5abea65406aea61240b0af4aede
Shwgupta@12 <guptashweta23797@gmail.com>
2022-06-04 00:31:04 +0845

    uc2 added

--- Git Demo/Git Demo/Program.cs
+++ Git Demo/Git Demo/Program.cs
@@ -7,6 +7,7 @@ namespace Git_Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World......!");
+            Console.WriteLine("yes fine!");
         }
     }
 }
